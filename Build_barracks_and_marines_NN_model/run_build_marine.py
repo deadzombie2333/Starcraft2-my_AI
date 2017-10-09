d9d11d48ac81526484133fad4f7583e5bac9f784
deadzombie2333 <32643904+deadzombie2333@users.noreply.github.com>
2017-10-09 10:45:09 -0500

    Delete run_build_marine.py

--- Build_barracks_and_marines_NN_model/run_build_marine.py
+++
@@ -1,8 +0,0 @@
-import numpy 
-import os
-import re
-import struct
-
-os.system("python -m pysc2.bin.agent \
-	--map BuildMarines \
-	--agent pysc2.agents.Build_barracks_and_marines_NN_model.my_agent.Build_Marine")
