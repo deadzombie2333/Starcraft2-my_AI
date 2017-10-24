838fcbd94c11878dc57cef017915f1ef3940aa12
Jingwei Gan <32643904+deadzombie2333@users.noreply.github.com>
2017-10-24 14:59:45 -0500

    Update best_defeat_lings_model.py

--- Defeat_zergling_and_banlings_NN_model/Test_best/best_defeat_lings_model.py
+++ Defeat_zergling_and_banlings_NN_model/Test_best/best_defeat_lings_model.py
@@ -3,42 +3,38 @@ import os
 import re
 import struct
 
-_NUM_POPULATION_SIZE = 10
+_NUM_POPULATION_SIZE = 1
+_NUM_TESTS = 5
 #generate a for loop for multiple generation of genetic algorithm
 
-try:
-  Threat_matrix_a_0 = numpy.load('best_threat_a_0.npy').item()
-except FileNotFoundError:
-  Threat_matrix_a_0 = numpy.load('threat_a_0.npy').item()
-  Threat_matrix_a_1 = numpy.load('threat_a_1.npy').item()
-  Move_matrix_a_0 = numpy.load('move_a_0.npy').item()
-  Move_matrix_a_1 = numpy.load('move_a_1.npy').item()
+Threat_matrix_a_0 = numpy.load('threat_a_0.npy').item()
+Threat_matrix_a_1 = numpy.load('threat_a_1.npy').item()
+Move_matrix_a_0 = numpy.load('move_a_0.npy').item()
+Move_matrix_a_1 = numpy.load('move_a_1.npy').item()
 
-  Score = numpy.load('score.npy')
+Score = numpy.load('score.npy')
 
-  good_index = numpy.argmax(numpy.mean(Score,axis=1))
-  Best_Threat_matrix_a_0 = {}
-  Best_Threat_matrix_a_1 = {}
-  Best_Move_matrix_a_0 = {}
-  Best_Move_matrix_a_1 = {}
+good_index = numpy.argmax(numpy.mean(Score,axis=1))
+Best_Threat_matrix_a_0 = {}
+Best_Threat_matrix_a_1 = {}
+Best_Move_matrix_a_0 = {}
+Best_Move_matrix_a_1 = {}
 
-  for item in range(_NUM_POPULATION_SIZE):
-    Best_Threat_matrix_a_0 [item] = Threat_matrix_a_0 [good_index]
-    Best_Threat_matrix_a_1 [item] = Threat_matrix_a_1 [good_index]
-    Best_Move_matrix_a_0 [item] = Move_matrix_a_0 [good_index]
-    Best_Move_matrix_a_1 [item] = Move_matrix_a_1 [good_index]
+for item in range(_NUM_POPULATION_SIZE):
+  Best_Threat_matrix_a_0 [item] = Threat_matrix_a_0 [good_index]
+  Best_Threat_matrix_a_1 [item] = Threat_matrix_a_1 [good_index]
+  Best_Move_matrix_a_0 [item] = Move_matrix_a_0 [good_index]
+  Best_Move_matrix_a_1 [item] = Move_matrix_a_1 [good_index]
 
-  Best_score = numpy.zeros((_NUM_POPULATION_SIZE,1))
-  numpy.save('best_NN_score',Best_score)
-  
-  numpy.save('best_threat_a_0',Best_Threat_matrix_a_0)
-  numpy.save('best_threat_a_1',Best_Threat_matrix_a_1)
-  numpy.save('best_move_a_0',Best_Move_matrix_a_0)
-  numpy.save('best_move_a_1',Best_Move_matrix_a_1)
+Best_score = numpy.zeros((_NUM_POPULATION_SIZE,_NUM_TESTS))
+numpy.save('best_NN_score',Best_score)
 
-os.system("python -m pysc2.bin.agent \
-  --map DefeatZerglingsAndBanelings \
-  --agent pysc2.agents.Test_lings_model.best_of_my_agent.Attack_Zerg")
+numpy.save('best_threat_a_0',Best_Threat_matrix_a_0)
+numpy.save('best_threat_a_1',Best_Threat_matrix_a_1)
+numpy.save('best_move_a_0',Best_Move_matrix_a_0)
+numpy.save('best_move_a_1',Best_Move_matrix_a_1)
+
+os.system("python -m pysc2.bin.agent --map DefeatZerglingsAndBanelings --agent pysc2.agents.Test_lings_model.best_of_my_agent.Attack_Zerg")
 
 #get score for each candidate
 Best_score = numpy.load('best_NN_score.npy')
